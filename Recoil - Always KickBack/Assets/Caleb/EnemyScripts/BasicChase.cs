5445a39e79060e7225778eb869251820621393fb
DJWooday <DownToTheLastPixel@gmail.com>
2019-03-24 13:29:59 -0500

    Added Caleb's enemy into the main scene, and added a chase movement to him.

---
+++ Recoil - Always KickBack/Assets/Caleb/EnemyScripts/BasicChase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicChase : MonoBehaviour {
+
+    [SerializeField]
+    private float sightDistance = 10;
+    [SerializeField]
+    private Transform character;
+
+    private NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+        navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Raycast to character
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, (character.position - transform.position));
+        // If it hits, check if it hit character
+        if (Physics.Raycast(ray, out hit, sightDistance)) {
+            if (hit.transform.tag == "Player") {
+                navAgent.destination = hit.transform.position;
+            }
+        }
+	}
+}
