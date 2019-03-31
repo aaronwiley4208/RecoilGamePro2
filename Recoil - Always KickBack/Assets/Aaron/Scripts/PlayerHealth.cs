e53831ecf786fd36bab849c9c6907e0f907d46aa
aaronwiley4208 <35614460+aaronwiley4208@users.noreply.github.com>
2019-03-31 16:52:36 -0500

    player prefab and health some

---
+++ Recoil - Always KickBack/Assets/Aaron/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float HP = 3;
+    public ParticleSystem deathExpl;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (HP <= 0) {
+            print("D E A D T");
+            deathExpl.enableEmission = true;
+        }
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "enemy") {
+            HP = HP - 1;
+            print(HP);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("Bullet")){
+            HP = HP - 1;
+            print(HP);
+        }
+    }
+}
