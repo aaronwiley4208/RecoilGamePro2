d05153c8ae98306a1b689ca2d4e3264e8d1cdbc2
Nicholas Fisher <fisher.nicholas@gmail.com>
2019-03-31 10:39:52 -0500

    shoot
    simple shoot behaviour implemented
    
    need to tie in state machine and object pooler

---
+++ Recoil - Always KickBack/Assets/Nick/nick_Scripts/newShoot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newShoot : MonoBehaviour
+{
+
+    public Transform player;
+    public float range = 50.0f;
+    public float bulletImpulse = 20.0f;
+    public GameObject cubePrefab;
+    public ObjectPooler ObjectPooler;
+    public int size;
+
+    private bool onRange = false;
+
+    public Rigidbody projectile;
+
+    void Start()
+    {
+        float rand = Random.Range(1.0f, 2.0f);
+        InvokeRepeating("Shoot", 2, rand);
+        ObjectPooler = new ObjectPooler(cubePrefab, size);
+    }
+
+    void Shoot()
+    {
+
+        if (onRange)
+        {
+            /*GameObject temp = ObjectPooler.Instance.GetObjectFromPool();
+            temp.SetActive(true);
+            temp.transform.position = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50));*/
+            Rigidbody bullet = (Rigidbody)Instantiate(projectile, transform.position + transform.forward, transform.rotation);
+            bullet.AddForce(transform.forward * bulletImpulse, ForceMode.Impulse);
+
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        onRange = Vector3.Distance(transform.position, player.position) < range;
+
+        if (onRange)
+            transform.LookAt(player);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
