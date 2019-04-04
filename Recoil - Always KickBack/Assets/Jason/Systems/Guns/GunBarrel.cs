98ef093b57dbb602597beac7c2bdf24a592158b6
Jason VR Rig\Jason Woodworth <DownToTheLastPixel@gmail.com>
2019-04-04 00:46:40 -0500

    Added spread control for bullets

--- Recoil - Always KickBack/Assets/Jason/Systems/Guns/GunBarrel.cs
+++ Recoil - Always KickBack/Assets/Jason/Systems/Guns/GunBarrel.cs
@@ -22,21 +22,65 @@ public class GunBarrel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Check input, if click, fire a bullet
-        if (Input.GetMouseButtonDown(0)) {
-            // Get direction TODO: Link this to the direction from the recoil script
-            Vector3 mouseScreenPos = Input.mousePosition;
-            mouseScreenPos.z = transform.position.z - Camera.main.transform.position.z;
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-            Vector3 mouseToPlayer = (mousePos - transform.position).normalized;
+        // Get direction TODO: Link this to the direction from the recoil script
+        Vector3 mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = transform.position.z - Camera.main.transform.position.z;
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        Vector3 mouseToPlayer = (mousePos - transform.position).normalized;
 
+        // Check input, if click, fire a bullet
+        if (Input.GetMouseButtonDown(0)) {
             Fire(mouseToPlayer);
         }
+
+        VisualizeSpread(mouseToPlayer);
 	}
 
     private void Fire(Vector3 direction) {
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(direction));
-        Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
-        bulletRB.AddForce(direction * bulletForce, ForceMode.Impulse);
+        // We need to determine the spread if there are multiple bullets
+        Vector3[] directions = ComputeSpread(direction);
+
+        for (int i = 0; i < bulletsPerClick; i++) {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(directions[i]));
+            Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
+            bulletRB.AddForce(directions[i] * bulletForce, ForceMode.Impulse);
+        }
+    }
+
+    /// <summary>
+    /// Compute the directions for a bullet spread pattern.
+    /// The angle will be spread around the initial direction, 
+    /// then revolve through the resulting wedge to find other directions.
+    /// </summary>
+    /// <param name="startDirection">The initial (mouse click) direction</param>
+    /// <returns>A series of directions</returns>
+    private Vector3[] ComputeSpread(Vector3 startDirection) {
+        Vector3[] directions = new Vector3[bulletsPerClick];
+        // The interval between bullet directions
+        float spreadIncrements = spreadAngle / (bulletsPerClick-1);
+        // The first direction, we'll spread from here
+        Vector3 initialDir = Quaternion.AngleAxis(spreadAngle / 2, Vector3.forward) * startDirection;
+        // Go through all the bullets, creating a dir for them all
+        for (int i = 0; i < bulletsPerClick; i++) {
+            directions[i] = Quaternion.AngleAxis(-spreadIncrements * i, Vector3.forward) * initialDir;
+        }
+        return directions;
+    }
+
+    private void VisualizeSpread(Vector3 startDirection) {
+        Debug.DrawRay(transform.position, startDirection, Color.green);
+
+        float halfSpread = spreadAngle / 2;
+
+        Vector3 topVec = Quaternion.AngleAxis(halfSpread, Vector3.forward) * startDirection;
+        Vector3 bottomVec = Quaternion.AngleAxis(-halfSpread, Vector3.forward) * startDirection;
+        //Vector3 topVec = new Vector3(startDirection.x, startDirection.magnitude * Mathf.Cos(halfSpread), startDirection.magnitude * Mathf.Sin(halfSpread));
+        Debug.DrawRay(transform.position, topVec, Color.red);
+        Debug.DrawRay(transform.position, bottomVec, Color.blue);
+
+        float spreadIncrements = spreadAngle / (bulletsPerClick-1);
+        for (int i = 0; i < bulletsPerClick; i++) {
+            Debug.DrawRay(transform.position, Quaternion.AngleAxis(-spreadIncrements * i, Vector3.forward) * topVec);
+        }
     }
 }
