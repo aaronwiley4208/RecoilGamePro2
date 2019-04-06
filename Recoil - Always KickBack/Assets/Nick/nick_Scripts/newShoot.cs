efadcfde074b8b978240a819e83c9b7c408baacf
Nicholas Fisher <fisher.nicholas@gmail.com>
2019-04-06 11:24:31 -0500

    raycast fixed
    figured out how to fix a raycast issue I was having. the bullets being fired were getting caught in the raycast
    
    Enemy line of sight is working in a basic way.

--- Recoil - Always KickBack/Assets/Nick/nick_Scripts/newShoot.cs
+++ Recoil - Always KickBack/Assets/Nick/nick_Scripts/newShoot.cs
@@ -10,33 +10,58 @@ public class newShoot : MonoBehaviour
     public float bulletImpulse = 10.0f;   
     private bool onRange = false;
     private Transform player;
-
+    private int layerMask;
+    private RaycastHit hit;
 
     private void Awake()
     {
-         player = GameObject.FindWithTag("Player").transform;
+        player = GameObject.FindWithTag("Player").transform;
+        int enemyLayerIndex = LayerMask.NameToLayer("Enemies");
+        int bulletLayerIndex = LayerMask.NameToLayer("Bullets");
+        layerMask = (1 << enemyLayerIndex) | (1 << bulletLayerIndex);
+        layerMask = ~layerMask;
     }
     void Start()
-    {
-        float rand = Random.Range(1.0f, 2.0f);
+    {        
+        float rand = Random.Range(1.5f, 2.0f);
         InvokeRepeating("Shoot", 2, rand);       
     }
 
     void Shoot()
     {
         if (onRange)
-        {            
+        {
             GameObject temp = ObjectPool.SharedInstance.GetPooledObject(projectile.tag);
             temp.transform.position = transform.position;
             temp.transform.rotation = transform.rotation;
             temp.SetActive(true);
             temp.transform.GetComponent<Rigidbody>().AddForce((player.transform.position - transform.position).normalized * bulletImpulse, ForceMode.Impulse);
+            
         }
     }
 
     void Update()
-    {
-        //boolean to check if player is within attack range
-        onRange = Vector3.Distance(transform.position, player.position) < range;
+    {                       
+        if (Physics.Raycast(transform.position, (player.transform.position - transform.position).normalized, out hit, range, layerMask))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                onRange = true;
+            }
+            else
+            {
+                onRange = false;
+            }
+        }
+
+        if(onRange == true)
+        {
+            Debug.DrawLine(transform.position, player.transform.position, Color.green);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, player.transform.position, Color.red);
+        }
+
     }
 }
