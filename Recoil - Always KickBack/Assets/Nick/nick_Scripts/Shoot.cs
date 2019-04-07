2b895ad2f0dac55489970cbc30213e4a8a565379
Nicholas Fisher <fisher.nicholas@gmail.com>
2019-04-07 18:48:10 -0500

    shoot script combine
    decided to combine shoot and lob scripts. a lot of the code was similar. eliminated the lob script entirely with 1 bool check.
    
    added colors to bullets for effect

--- Recoil - Always KickBack/Assets/Nick/nick_Scripts/Shoot.cs
+++ Recoil - Always KickBack/Assets/Nick/nick_Scripts/Shoot.cs
@@ -6,6 +6,7 @@ public class Shoot : MonoBehaviour
 {
     public GameObject projectile;
     public float range = 50.0f;
+    public bool isLob = false;
     public float bulletImpulse = 10.0f;
     private Transform player;
     private int layerMask;
@@ -21,8 +22,8 @@ public class Shoot : MonoBehaviour
 
     void Start()
     {
-        float rand = Random.Range(1.0f, 2.0f);
-        StartCoroutine("Fire", rand);       
+        float rand = Random.Range(1.0f, 2.0f);        
+        StartCoroutine("Fire", rand);
     }
 
     void Update()
@@ -43,11 +44,29 @@ public class Shoot : MonoBehaviour
                 GameObject temp = ObjectPool.SharedInstance.GetPooledObject(projectile.tag);
                 temp.transform.position = transform.position;
                 temp.transform.rotation = transform.rotation;
-                temp.SetActive(true);
-                temp.transform.GetComponent<Rigidbody>().AddForce((player.transform.position - transform.position).normalized * bulletImpulse, ForceMode.Impulse);
-
+                temp.SetActive(true);                
+                Fire(temp);
             }
             yield return new WaitForSeconds(rand);
+        }
+    }
+
+    public void Fire(GameObject obj)
+    {
+        if(isLob == true)
+        {
+            var dir = player.position - transform.position; // get target direction
+            var h = dir.y;  // get height difference
+            dir.y = 0;  // retain only the horizontal direction
+            var dist = dir.magnitude;  // get horizontal distance
+            dir.y = dist;  // set elevation to 45 degrees
+            dist += h;  // correct for different heights
+            var vel = Mathf.Sqrt(dist * Physics.gravity.magnitude);
+            obj.transform.GetComponent<Rigidbody>().velocity = vel * dir.normalized;
+        }
+        else
+        {
+            obj.transform.GetComponent<Rigidbody>().AddForce((player.transform.position - transform.position).normalized * bulletImpulse, ForceMode.Impulse);
         }
     }
 }
