823529d91be8caca4028bd9ccb0723dbb5ae390e
Nicholas Fisher <fisher.nicholas@gmail.com>
2019-04-10 20:25:51 -0500

    enemy health
    simple enemy health script
    
    changed bullet prefab from players guns to "Player Bullet" tag and "Player Bullet" layer.
    
    changed physics matrix to ignore bullets in air

---
+++ Recoil - Always KickBack/Assets/Nick/nick_Scripts/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int health = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.collider);
+        if (col.collider.tag == "Player Bullet")
+        {
+            health -= 1;
+            Debug.Log("Hit");
+        }       
+        
+    }
+
+}
