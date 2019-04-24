e9323c761d6640e0e20bb76f8ae43aea0d59414c
Nicholas Fisher <fisher.nicholas@gmail.com>
2019-04-24 11:23:11 -0500

    Ferr2D level

---
+++ Recoil - Always KickBack/Assets/Nick/nick_Scripts/SimpleMovementRigidbody.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovementRigidbody : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        }
+
+    }
+}
