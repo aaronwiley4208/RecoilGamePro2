f6e9940036ae931428a949564ef70ccab427acbc
Nicholas Fisher <fisher.nicholas@gmail.com>
2019-04-29 21:45:10 -0500

    enemy prefab glow and sound

---
+++ Recoil - Always KickBack/Assets/Nick/nick_Scripts/PulseNeonIntensity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseNeonIntensity : MonoBehaviour {
+
+    [SerializeField]
+    private float intensity;
+    private Color emissionColor;
+    private Material material;
+    private Color newEmission;
+    [SerializeField]
+    private float minIntensity;
+    [SerializeField]
+    private float timePerPeriod;
+    [SerializeField]
+    private float intensityDampener;
+
+	// Use this for initialization
+	void Start () {
+        material = GetComponent<Renderer>().material;
+        emissionColor = material.GetColor("_EmissionColor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        intensity = minIntensity + 1 + (Mathf.Sin((Time.time * 2 * Mathf.PI) / timePerPeriod)) / intensityDampener;
+        newEmission = emissionColor * intensity;
+
+        material.SetColor("_EmissionColor", newEmission);
+	}
+}
